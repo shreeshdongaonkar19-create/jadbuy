d6a93b25dec352cd1771cb6ec7fec9aad1b3d4c4
Shreesh Dongaonkar <shreeshdongaonkar19@gmail.com>
2026-06-08 15:49:03 +0530

    intial commit

---
+++ file.js
@@ -0,0 +1,22 @@
+document.addEventListener('keydown', function(event) {
+	console.log(event.key);
+		if(event.key == 'a'){
+			window.location.href="file:///C:/Users/Shreesh/Documents/my%20main%20taks%20for%20work/js%20tasks/event%20%20listner/web/about.html";
+		}
+		
+		if(event.key == 's'){
+			window.location.href="file:///C:/Users/Shreesh/Documents/my%20main%20taks%20for%20work/js%20tasks/event%20%20listner/web/Services.html";
+		}
+		
+		if(event.key == 'p'){
+			window.location.href="file:///C:/Users/Shreesh/Documents/my%20main%20taks%20for%20work/js%20tasks/event%20%20listner/web/pricing.html";
+		}
+		
+		if(event.key == 'c'){
+			window.location.href="file:///C:/Users/Shreesh/Documents/my%20main%20taks%20for%20work/js%20tasks/event%20%20listner/web/Contact.HTML";
+		}
+		
+		if(event.key == 'h'){
+			window.location.href="file:///C:/Users/Shreesh/Documents/my%20main%20taks%20for%20work/js%20tasks/event%20%20listner/web/website.html";
+		}
+			});
